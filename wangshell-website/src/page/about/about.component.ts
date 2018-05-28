1bc85c30e16e6b2f4f2816cfd26eb1976e07151f
Lemon <416183203@qq.com>
2018-05-28 18:06:05 +0800

    修改关于页面请求参数

--- wangshell-website/src/page/about/about.component.ts
+++ wangshell-website/src/page/about/about.component.ts
@@ -2,6 +2,7 @@ import { Title } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import { CommonHttpService } from '../../module/http/common-http.service';
+import { REQUEST_TYPE_ABOUT_US } from '../../app/app.global';
 
 @Component({
   selector: 'app-about',
@@ -13,7 +14,7 @@ export class AboutComponent implements OnInit {
   constructor(private title: Title, private commonService: CommonHttpService) {
     this.title.setTitle('关于我们 | 上海汪壳网络科技有限公司');
 
-    commonService.getDataInfo('1', (d) => {
+    commonService.getDataInfo(REQUEST_TYPE_ABOUT_US, (d) => {
       /** @todo 需要显示数据 */
     })
   }
