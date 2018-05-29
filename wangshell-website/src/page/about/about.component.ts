88a82dff5aedd3c9f64871d51f005ee2a691f159
fu <funaifan@126.com>
2018-05-29 22:30:17 +0800

    home&about对接完成

--- wangshell-website/src/page/about/about.component.ts
+++ wangshell-website/src/page/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Title } from '@angular/platform-browser';
+import { DataInfoModel } from '../../model/data-info.model'
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import { CommonHttpService } from '../../module/http/common-http.service';
@@ -11,17 +12,19 @@ import { REQUEST_TYPE_ABOUT_US } from '../../app/app.global';
 })
 export class AboutComponent implements OnInit {
 
-  public aboutData;
+   /** @name 关于我们数据 */
+  public aboutData:DataInfoModel;
   constructor(private title: Title, private commonService: CommonHttpService) {
     this.title.setTitle('关于我们 | 上海汪壳网络科技有限公司');
 
-    commonService.getDataInfo(REQUEST_TYPE_ABOUT_US, (d) => {
-      /** @todo 需要显示数据 */
-      this.aboutData = d.data_value;
-    })
+
   }
 
   ngOnInit() {
+    this.commonService.getDataInfo(REQUEST_TYPE_ABOUT_US, (d) => {
+      /** @todo 需要显示数据 */
+      this.aboutData = d.data_value;
+    })
     $('html,body').removeClass('h-show');
   }
 
